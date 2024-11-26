5e25d9242cb9d97b92254ae7dc4c65ac7de910cc
Sumeet Gupta <101032878+2021aim1014@users.noreply.github.com>
2024-11-26 11:20:17 +0530

    Update vgg19_backbone.py

--- src/models/vgg19_backbone.py
+++ src/models/vgg19_backbone.py
@@ -8,26 +8,26 @@ class VGG19_backbone(nn.Module):
         
         # Encoder: Downsampling
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),  # 2300x1800 -> 2300x1800
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 2300x1800 -> 1150x900
+            # nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),  # 2300x1800 -> 2300x1800
+            # nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=2),  # 2300x1800 -> 1150x900
             
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 1150x900 -> 1150x900
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 1150x900 -> 575x450
+            # nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),  # 1150x900 -> 1150x900
+            # nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=2),  # 1150x900 -> 575x450
             
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  # 575x450 -> 575x450
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 575x450 -> 287x225
+            # nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  # 575x450 -> 575x450
+            # nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=2),  # 575x450 -> 287x225
             
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),  # 287x225 -> 287x225
+            nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1),  # 287x225 -> 287x225
             nn.ReLU(),
             nn.AdaptiveAvgPool2d((224, 224))  # 287x225 -> 224x224
         )
         
         # Bottleneck
         self.bottleneck = nn.Sequential(
-            nn.Conv2d(128, 3, kernel_size=3, stride=1, padding=1),  # 224x224 -> 224x224
+            nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1),  # 224x224 -> 224x224
             nn.ReLU()
         )
 
