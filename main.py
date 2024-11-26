31de37b0e3b38ccf9074a94d6e395a5351fb8d14
Sumeet Gupta <101032878+2021aim1014@users.noreply.github.com>
2024-11-26 11:51:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,39 +14,14 @@ from src.train import train_model
 from src.models.vgg19_master_add import VGG19Master as VGG19Master_ADD
 from src.models.vgg19_master_concat import VGG19Master as VGG19Master_CONCAT
 from src.models.vgg19_backbone import VGG19_backbone
+from src.models.vgg_master_add_better import VGG19Master as VGG19MasterBetter
 
-# original_image_shape = (3, 224, 224)
-# image_shape = (3, 1800, 2300)
+original_image_shape = (3, 1800, 2300)
+image_shape = (3, 224, 224)
 
-# # Transformations (optional)
-# transform = transforms.Compose([
-#     transforms.Resize((image_shape[1], image_shape[2])),  # Resize to a common size
-#     transforms.ToTensor(),  # Convert image to PyTorch tensor
-# ])
-
-# # Instantiate the dataset and DataLoader
-# train_csv_file = 'dataset/meta_data_complete.csv'  # Replace with your CSV path
-# val_csv_file = 'dataset/validate_meta_data.csv'  # Replace with your CSV path
-# img_dir = 'dataset/removed_background'  # Replace with your image directory
-# train_dataset = ImageDataset(csv_file=train_csv_file, img_dir=img_dir, transform=transform)
-# val_dataset = ImageDataset(csv_file=val_csv_file, img_dir=img_dir, transform=transform)
-
-# print("Size of train dataset: {}".format(len(train_dataset)))
-# print("Size of test dataset: {}".format(len(val_dataset)))
-# print("Input test size: {}".format(val_dataset[0][0].size()))
-# print("Label test size: {}".format(val_dataset[0][1].size()))
-
-# train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=2)
-# val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=2)
-
-# model_name = 'BaseLineModel_224'
-# model = BaselineModel(input_size=image_shape, output_size=4, dropout_p = 0.8)
-
-# model_name = 'VGG19_finetune'
-# model = VGG19(input_size=(3, 224, 224), output_size=4, finetune=True)
-
-# # Transformations (optional)
+# Transformations (optional)
 transform = transforms.Compose([
+    transforms.Resize((image_shape[1], image_shape[2])),  # Resize to a common size
     transforms.ToTensor(),  # Convert image to PyTorch tensor
 ])
 
@@ -62,11 +37,42 @@ print("Size of test dataset: {}".format(len(val_dataset)))
 print("Input test size: {}".format(val_dataset[0][0].size()))
 print("Label test size: {}".format(val_dataset[0][1].size()))
 
-train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=0)
-val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=0)
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=2)
+val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=2)
 
-model_name = 'VGG19_master_backbone'
-model = VGG19_backbone(input_size=(3, 2300, 1800), output_size=4)
+model_name = 'VGG19_master_add_better'
+model = VGG19MasterBetter(num_classes=4, img_shape=(3, 224, 224))
+
+# model_name = 'BaseLineModel_224'
+# model = BaselineModel(input_size=image_shape, output_size=4, dropout_p = 0.8)
+
+# model_name = 'VGG19_finetune'
+# model = VGG19(input_size=(3, 224, 224), output_size=4, finetune=True)
+
+# *********************************************************
+# # Transformations (optional)
+# transform = transforms.Compose([
+#     transforms.ToTensor(),  # Convert image to PyTorch tensor
+# ])
+
+# # Instantiate the dataset and DataLoader
+# train_csv_file = 'dataset/meta_data_complete.csv'  # Replace with your CSV path
+# val_csv_file = 'dataset/validate_meta_data.csv'  # Replace with your CSV path
+# img_dir = 'dataset/removed_background'  # Replace with your image directory
+# train_dataset = ImageDataset(csv_file=train_csv_file, img_dir=img_dir, transform=transform)
+# val_dataset = ImageDataset(csv_file=val_csv_file, img_dir=img_dir, transform=transform)
+
+# print("Size of train dataset: {}".format(len(train_dataset)))
+# print("Size of test dataset: {}".format(len(val_dataset)))
+# print("Input test size: {}".format(val_dataset[0][0].size()))
+# print("Label test size: {}".format(val_dataset[0][1].size()))
+
+# train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=0)
+# val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=0)
+
+# model_name = 'VGG19_master_backbone'
+# model = VGG19_backbone(input_size=(3, 2300, 1800), output_size=4)
+# *********************************************************
 
 # Loss function and optimizer
 criterion = nn.MSELoss()  # For regression (use BCEWithLogitsLoss for multi-label classification)
